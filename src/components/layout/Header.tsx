079b75a3cf2c66e2b7f6b4f650a8b761f400e158
adrien-tello <161447856+adrien-tello@users.noreply.github.com>
2025-06-26 13:16:49 +0100

    updated header1

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -23,6 +23,7 @@ const Header = () => {
           </button>
           <Link to="/" className="text-2xl font-bold flex items-center">
             <span className="mr-2">     
+                <img src="/src/assets/HomeChef_Logo.png" alt="logo" width={100} />
             </span>
           </Link>
         </div>
